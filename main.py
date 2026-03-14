ea32dd93300f9c25c412c540cb2ad52ee8d1e538
Arul <cs24i1027@iiitdm.ac.in>
2026-03-14 16:30:07 +0530

    using fast api, query parameters, to put the link in the browser to get the qr code in browser itself

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 import qrcode
+import io
 
-url = input("Enter the URL: ").strip()
-file_path = "qrcode.png"
+app=FastAPI()
+@app.get("/")
+def home():
+    return {"message":"Welcome to QR code generator API. Use /qr?url=your_url to generate QR code."}
 
-qr = qrcode.QRCode()
-qr.add_data(url)
+@app.get("/qr")
+def generate_qr(url: str):
+    qr = qrcode.QRCode()
+    qr.add_data(url)
+    img = qr.make_image()
 
-img = qr.make_image()
-img.save(file_path)
+    buffer = io.BytesIO() #create a bytes buffer to hold the image data
+    img.save(buffer, format="PNG") #save the image to the buffer in PNG format
+    buffer.seek(0)
 
-print("QR code is generated and saved as", file_path)
-
-
-
+    return StreamingResponse(buffer, media_type="image/png")
