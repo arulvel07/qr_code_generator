86a8401053d54d40fdf33f264f2d8fbe776b5dfe
Arul <cs24i1027@iiitdm.ac.in>
2026-03-15 11:40:34 +0530

    connected backend with frontend using form method get

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, FileResponse
 import qrcode
 import io
 
-app=FastAPI()
+app = FastAPI()
+
 @app.get("/")
 def home():
-    return {"message":"Welcome to QR code generator API. Use /qr?url=your_url to generate QR code."}
+    return FileResponse("index.html", media_type="text/html")
 
 @app.get("/qr")
 def generate_qr(url: str):
@@ -14,8 +15,8 @@ def generate_qr(url: str):
     qr.add_data(url)
     img = qr.make_image()
 
-    buffer = io.BytesIO() #create a bytes buffer to hold the image data
-    img.save(buffer, format="PNG") #save the image to the buffer in PNG format
+    buffer = io.BytesIO()  # create a bytes buffer to hold the image data
+    img.save(buffer, format="PNG")  # save the image to the buffer in PNG format
     buffer.seek(0)
 
     return StreamingResponse(buffer, media_type="image/png")
